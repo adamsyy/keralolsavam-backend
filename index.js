9e3a43e4573a3ea03d4edac92978f62811b3033c
jaison080 <jaisondennis080@gmail.com>
2022-12-01 06:45:38 +0530

    Updated

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+const { spawn } = require("child_process");
+try {
+  setInterval(() => {
+    const child = spawn(`git pull --rebase origin master`, { shell: true });
+    child.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+  
+    child.stderr.on("data", (data) => {
+      console.log(`stderr: ${data}`);
+    });
+  
+    child.on("error", (error) => {
+      console.log(`error: ${error.message}`);
+    });
+  
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }, 2000);
+}
+catch(err) {
+  console.log(err);
+}
+
+
+
 const express = require("express");
 require('dotenv').config()
 const app = express();
