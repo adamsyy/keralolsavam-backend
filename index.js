0e3ab9cfcfb6909dd002195ddda0e79f8784f196
Jaison Dennis <jaisondennis080@gmail.com>
2022-12-01 06:41:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors())
 
 //send response api is working 
 app.get("/", (req, res) => {
-  res.send("API is working.");
+  res.send("API is working perfectly.");
 });
 
 app.use("/api/auth", require("./routes/auth"));
