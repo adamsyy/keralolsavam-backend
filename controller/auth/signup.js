e3df5b89ff0f512d665c30f3096ede3f788afe5e
jaison080 <jaisondennis080@gmail.com>
2022-12-04 13:36:58 +0530

    Updated

--- controller/auth/signup.js
+++ controller/auth/signup.js
@@ -1,22 +1,24 @@
 const UserSchema = require("../../models/User");
 
-module.exports.signup=async(req,res)=>{
-    var {name,address,sex,location,panchayat}=req.body; 
+module.exports.signup = async (req, res) => {
+  var { name, address, sex, place, lsgi,localbody, artEvents, sportsEvents } = req.body;
 
-    
-const user=new UserSchema({
+  const user = new UserSchema({
     name,
     address,
     sex,
-    location,
-    panchayat
-})
-user.save()
-.then((user)=>{
-    res.status(200).json({message:"user saved",user})
-})
-.catch((err)=>{
-    res.status(400).json({message:"error",err})
-}
-)
-}
+    place,
+    lsgi,
+    localbody,
+    artEvents,
+    sportsEvents,
+  });
+  user
+    .save()
+    .then((user) => {
+      res.status(200).json({ message: "user saved", user });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "error", err });
+    });
+};
--- models/User.js
+++ models/User.js
@@ -1,34 +1,32 @@
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const User = new Schema({
+  name: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  sex: {
+    type: String,
+  },
+  place: {
+    type: String,
+  },
+  lsgi: {
+    type: String,
+  },
+  localbody: {
+    type: String,
+  },
+  artEvents:{
+    type: [String],
+  },
+  sportsEvents:{
+    type: [String],
+  },
+});
 
-const User= new Schema({
-    name:{
-        type:String,
-    
-      },
-      address:
-        {
-            type:String,
-        },
-        sex:
-        {
-            type:String,
-        },
-        location:
-        {
-            type:String,
-        },
-        panchayat:
-        {
-            type:String,
-        },
-        
-  
-  
-  });
-
-  const UserSchema = mongoose.model("User", User);
+const UserSchema = mongoose.model("User", User);
 module.exports = UserSchema;
-  
