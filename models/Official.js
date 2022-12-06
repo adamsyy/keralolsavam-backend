7afd10bdef8f2535e4fe7035699588fb6c6bcd8d
adamsyy <adamoommenjacob@gmail.com>
2022-12-06 14:37:01 +0530

    hii

---
+++ models/Official.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Official = new Schema({
+ 
+ 
+
+});
+
+const OfficialSchema = mongoose.model("Official", Official);
+module.exports = OfficialSchema;
