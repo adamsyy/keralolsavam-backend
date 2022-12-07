42a3325bf0bcde57d57f54660d22f9964d8ea3a8
Jagannathes <jagannatheshahi@gmail.com>
2022-12-07 19:33:01 +0530

    bug fix

--- controller/auth/getAllUsers.js
+++ controller/auth/getAllUsers.js
@@ -2,7 +2,7 @@ const User = require("../../models/User");
 
 module.exports.getAllUsers = async (req, res) => {
   try {
-    const pagination = req.query.page ? req.query.page : 15;
+    const pagination = req.query.pagination ? req.query.pagination : 15;
     const pageNumber = req.query.page ? req.query.page : 1;
     const users = await User.find({})
       .skip((pageNumber - 1) * pagination)
