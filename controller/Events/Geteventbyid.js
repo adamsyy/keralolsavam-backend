69c17449e65827d45c6638e79e7cd10c3423133c
adamsyy <adamoommenjacob@gmail.com>
2022-12-10 09:41:48 +0530

    json fans

---
+++ controller/Events/Geteventbyid.js
@@ -0,0 +1,48 @@
+const EventsSchema = require('../../models/Events');
+const UserSchema = require('../../models/User');
+
+module.exports.Geteventbyid = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const event = await EventsSchema.findById(id);
+        //get details of users who have registered for the event
+
+        var users_list = [];
+
+        const user = await UserSchema.find();
+        for (var i = 0; i < user.length; i++) {
+            if(event.isarts==true){
+
+                for (var j = 0; j < user[i].artEvents.length; j++) {
+                    if (user[i].artEvents[j] == event.name) {
+                        users_list.push(user[i]);
+                    }
+                }
+            }
+            else{
+                for (var j = 0; j < user[i].sportsEvents.length; j++) {
+                    if (user[i].sportsEvents[j] == event.name) {
+                        users_list.push(user[i]);
+                    }
+                }
+            }
+           
+
+            
+        }
+
+        res.send(
+            {
+                "event": event,
+                "users": users_list
+            }
+        );
+    }
+    catch (err) {
+        res
+            .status(400)
+            .send(err);
+
+    }
+}
+
--- routes/Events.js
+++ routes/Events.js
@@ -1,11 +1,13 @@
 const {Getevents}=require('../controller/Events/Getevents');
 const {Postevents}=require('../controller/Events/Postevents');
 const {Addwinners}=require('../controller/Events/Addwinners');
+const {Geteventbyid}=require('../controller/Events/Geteventbyid');
 
 const router = require("express").Router();
 
 router.post("/", Postevents);
 router.get("/", Getevents);
 router.put("/", Addwinners);
+router.post("/id", Geteventbyid);
 
 module.exports = router;
