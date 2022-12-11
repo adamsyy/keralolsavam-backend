49fcb6f61260e35ff67dc811ece602da1c08412a
adamsyy <adamoommenjacob@gmail.com>
2022-12-11 23:14:57 +0530

    lsgi done

--- routes/score.js
+++ routes/score.js
@@ -1,5 +1,6 @@
 const {Score}=require('../controller/Score')
 const {Localbody}=require('../controller/Localbody')
+const {lsgi}=require('../controller/lsgi')
 
 
 const router = require("express").Router();
@@ -7,5 +8,6 @@ const router = require("express").Router();
 
 router.get('/',Score)
 router.get('/localbody',Localbody)
+router.get('/lsgi',lsgi)
 
 module.exports=router
