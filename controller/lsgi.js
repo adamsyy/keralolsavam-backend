4757b10f77717f3e6440cfb6691a0acb745dfdaf
jaison080 <jaisondennis080@gmail.com>
2022-12-11 23:44:59 +0530

    Updated

--- controller/lsgi.js
+++ controller/lsgi.js
@@ -1,76 +1,63 @@
 const ParticipantSchema = require("../models/partcipant");
 
 module.exports.lsgi = async (req, res) => {
-    var block = 0;
-    var block_arts = 0;
-    var block_sports = 0;
+  var block = 0;
+  var block_arts = 0;
+  var block_sports = 0;
 
-    var muncipality = 0;
-    var muncipality_arts = 0;
-    var muncipality_sports = 0;
+  var muncipality = 0;
+  var muncipality_arts = 0;
+  var muncipality_sports = 0;
 
-    var corporation = 0;
-    var corporation_arts = 0;
-    var corporation_sports = 0;
+  var corporation = 0;
+  var corporation_arts = 0;
+  var corporation_sports = 0;
 
+  const participants = await ParticipantSchema.find();
 
-    const participants = await ParticipantSchema.find();
-
-    for (var i = 0; i < participants.length; i++) {
-        if (participants[i].localbody == "Block Panchayath") {
-            block += parseInt(participants[i].score);
-            if (participants[i].isarts == true) {
-                block_arts += parseInt(participants[i].score);
-            }
-            else {
-                block_sports += parseInt(participants[i].score);
-            }
-
-
-        }
-
-        else if (participants[i].localbody == "Municipalities") {
-            muncipality += parseInt(participants[i].score);
-            if (participants[i].isarts == true) {
-                muncipality_arts += parseInt(participants[i].score);
-            }
-            else {
-                muncipality_sports += parseInt(participants[i].score);
-            }
-
-        }
-
-        else if (participants[i].localbody == "Corporation") {
-            corporation += parseInt(participants[i].score);
-            if (participants[i].isarts == true) {
-                corporation_arts += parseInt(participants[i].score);
-            }
-            else {
-                corporation_sports += parseInt(participants[i].score);
-            }
-
-        }
-
-
+  for (var i = 0; i < participants.length; i++) {
+    if (participants[i].localbody == "Block Panchayath") {
+      block += parseInt(participants[i].score);
+      if (participants[i].isarts == true) {
+        block_arts += parseInt(participants[i].score);
+      } else {
+        block_sports += parseInt(participants[i].score);
+      }
+    } else if (participants[i].localbody == "Municipalities") {
+      muncipality += parseInt(participants[i].score);
+      if (participants[i].isarts == true) {
+        muncipality_arts += parseInt(participants[i].score);
+      } else {
+        muncipality_sports += parseInt(participants[i].score);
+      }
+    } else if (participants[i].localbody == "Corporation") {
+      corporation += parseInt(participants[i].score);
+      if (participants[i].isarts == true) {
+        corporation_arts += parseInt(participants[i].score);
+      } else {
+        corporation_sports += parseInt(participants[i].score);
+      }
     }
+  }
 
-    res.send({
-        "block_panchayat":{
-            "total_score":block,
-            "arts_score":block_arts,
-            "sports_score":block_sports
-
-        },
-        "municipalities":{
-            "total_score":muncipality,
-            "arts_score":muncipality_arts,
-            "sports_score":muncipality_sports
-        },
-        "corporation":{
-            "total_score":corporation,
-            "arts_score":corporation_arts,
-            "sports_score":corporation_sports
-        }
-    })
-
-}
+  res.send([
+    {
+      name: "Block Panchayath",
+      total_score: block,
+      arts_score: block_arts,
+      sports_score: block_sports,
+    },
+     {
+      name: "Municipalities",
+      total_score: muncipality,
+      arts_score: muncipality_arts,
+      sports_score: muncipality_sports,
+    },
+     {
+      name: "Corporation",
+      total_score: corporation,
+      arts_score: corporation_arts,
+      sports_score: corporation_sports,
+    },
+]);
+};
